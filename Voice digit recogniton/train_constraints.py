0f4e796c3b967d7950641b3ba72ccd87c7068261
fmazilu <florinmazilu96@gmail.com>
2021-10-21 11:46:16 +0300

    Update train_constraints.py

--- Voice digit recogniton/train_constraints.py
+++ Voice digit recogniton/train_constraints.py
@@ -6,12 +6,11 @@ from tensorflow.keras.layers import Dense, BatchNormalization, Dropout, Input, L
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, Callback
 from tensorflow.keras.models import load_model
+from tensorflow.keras.losses import CategoricalCrossentropy
 import datetime
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import seaborn as sn
 from extract_features_construct_dataset import get_lipschitz_constrained
-from Constraints import customConstraint, norm_constraint, norm_constraint_FISTA
+from Constraints import customConstraint, norm_constraint, norm_constraint_FISTA, simple_norm_constraint
 
 # Loading the datasets
 path = 'processed_google_dataset/'
@@ -38,8 +37,8 @@ train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_label))
 val_dataset = tf.data.Dataset.from_tensor_slices((val_data, val_label))
 test_dataset = tf.data.Dataset.from_tensor_slices((test_data, test_label))
 
-train_dataset = train_dataset.shuffle(880, reshuffle_each_iteration=False).batch(256)
-val_dataset = val_dataset.shuffle(880, reshuffle_each_iteration=False).batch(256)
+train_dataset = train_dataset.shuffle(880, reshuffle_each_iteration=False).batch(512)
+val_dataset = val_dataset.shuffle(880, reshuffle_each_iteration=False).batch(512)
 
 
 def tensorboard_callback():
@@ -49,30 +48,41 @@ def tensorboard_callback():
 
 
 # Callback class for monitoring the Lipschitz constant
-class lip_stats_callback(Callback):
+class lip_stats_callback(Callback): ## sa printeze si norma fiecarei ponderi in parte, min max pe fiecare pondere + google colab
     def on_epoch_begin(self, epoch, logs=None):
         lip_cst = get_lipschitz_constrained(model)
+        for layer in self.model.layers:
+            if 'dense' in layer.name:
+                w = layer.get_weights()[0]
+                norm = np.linalg.norm(w, ord=2)
+                print(f"The norm for layer {layer} is : {norm}")
         print(f'The Lipschitz constant on epoch {epoch} is {lip_cst}')
 
 
 ## cst < 5; test_acc 85%
 def get_model():
-    m = 6
+    m = 5
+    rho = 20
     inp = Input((880,))
-    hdn = Dense(256, activation='relu')(inp)#, kernel_constraint=customConstraint(rho=np.power(rho, 1/m)))(inp)
-    hdn = Dropout(0.2)(hdn)
-    # hdn = Dense(256, activation='relu')(hdn)
-    # hdn = Dropout(0.1)(hdn)
-    hdn = Dense(128, activation='relu')(hdn)
-    # hdn = Dropout(0.1)(hdn)
-    # hdn = Dense(256, activation='relu', kernel_constraint=NonNeg())(hdn)
-    # hdn = Dropout(0.2)(hdn)
-    # hdn = Dense(128, activation='relu', kernel_constraint=NonNeg())(hdn)
-    # hdn = Dropout(0.2)(hdn)
-    # hdn = Dense(128, activation='relu', kernel_constraint=NonNeg())(hdn)
-    hdn = Dense(64, activation='relu')(hdn)
-    hdn = Dense(32, activation='relu')(hdn)
-    out = Dense(10, activation='softmax')(hdn)
+    hdn = Dense(1024, activation='relu', kernel_constraint=NonNeg())(inp)
+    hdn = BatchNormalization()(hdn)
+    hdn = Dropout(0.5)(hdn)
+
+    hdn = Dense(512, activation='relu', kernel_constraint=NonNeg())(hdn)
+    hdn = BatchNormalization()(hdn)
+    hdn = Dropout(0.4)(hdn)
+
+    hdn = Dense(256, activation='relu', kernel_constraint=NonNeg())(hdn)
+    hdn = BatchNormalization()(hdn)
+    hdn = Dropout(0.4)(hdn)
+
+    hdn = Dense(128, activation='relu', kernel_constraint=NonNeg())(hdn)
+    hdn = BatchNormalization()(hdn)
+
+    hdn = Dense(64, activation='relu', kernel_constraint=NonNeg())(hdn)
+    hdn = BatchNormalization()(hdn)
+
+    out = Dense(10, activation='softmax', kernel_constraint=NonNeg())(hdn)
 
     model = Model(inputs=inp, outputs=out)
     return model
@@ -81,18 +91,20 @@ def get_model():
 if __name__ == '__main__':
     #### Model Training
     model = get_model()
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=CategoricalCrossentropy(), metrics=['accuracy'])
     print(model.summary())
     # model.load_weights('bin/models_constrained/model_1layer.h5')
     model.fit(train_dataset, epochs=2000, validation_data=val_dataset, verbose=2,
               callbacks=[tensorboard_callback(),
-                         EarlyStopping(monitor="val_loss", patience=600, restore_best_weights=False),
-                         norm_constraint_FISTA(rho=10, nit=10),
-                         # norm_constraint(rho=10),
+                         EarlyStopping(monitor="val_loss", patience=1000, restore_best_weights=False),
+                         # norm_constraint_FISTA(rho=5, nit=2),
+                         # norm_constraint(rho=10),     ## Read more about layer indices
+                         # simple_norm_constraint(rho=10, affected_layers_indices=[5,6]),
                          lip_stats_callback(),
-                         ModelCheckpoint('bin/models_constrained/model_4layerFISTA_rho10_droupout.h5', save_best_only=True, verbose=1)])
+                         ModelCheckpoint('bin/models_constrained/model_test.h5',
+                                         save_best_only=True, verbose=1)]) #model_5layerFISTA_rho20_droupout
 
-    # model = load_model("bin\models_constrained\\model_Dropout1_moreLayersV2.h5")
+    model = load_model("bin/models_constrained/model_test.h5")
     y = np.argmax(model.predict(test_data), axis=1)
     results = model.evaluate(test_data, test_label)
     print(f'Test loss: {results[0]} / Test accuracy: {results[1]}')
